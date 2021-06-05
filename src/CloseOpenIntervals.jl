b0bd0ca1460be726cf8218b2d98f81982d3a5822
chriselrod <elrodc@gmail.com>
2021-06-05 12:11:00 -0400

    Create repo

--- src/CloseOpenIntervals.jl
+++ src/CloseOpenIntervals.jl
@@ -1,5 +1,42 @@
 module CloseOpenIntervals
 
-# Write your package code here.
+using Static: StaticInt, Zero, One
+export CloseOpen, SafeCloseOpen
+
+abstract type AbstractCloseOpen{L <: Integer, U <: Integer} <: AbstractUnitRange{Int} end
+for T ∈ (:CloseOpen,:SafeCloseOpen)
+  @eval begin
+    struct $T{L <: Integer, U <: Integer} <: AbstractCloseOpen{L,U}
+      start::L
+      upper::U
+      @inline $T{L,U}(l::L,u::U) where {L <: Integer, U <: Integer} = new{L,U}(l,u)
+    end
+    @inline $T(s::S, u::U) where {S,U} = $T{S,U}(s, u)
+    @inline $T(len::T) where {T<:Integer} = $T{Zero,T}(Zero(), len)
+  end
+end
+
+@inline Base.first(r::AbstractCloseOpen) = getfield(r,:start)
+@inline Base.first(r::AbstractCloseOpen{StaticInt{F}}) where {F} = StaticInt{F}()
+@inline Base.step(::AbstractCloseOpen) = One()
+@inline Base.last(r::AbstractCloseOpen{<:Integer,I}) where {I} = getfield(r,:upper) - oneunit(I)
+@inline Base.last(r::AbstractCloseOpen{<:Integer,StaticInt{L}}) where {L} = StaticInt{L}() - One()
+@inline Base.length(r::AbstractCloseOpen) = getfield(r,:upper) - getfield(r,:start)
+@inline Base.length(r::AbstractCloseOpen{Zero}) = getfield(r,:upper)
+
+@inline Base.iterate(r::CloseOpen) = (i = Int(first(r)); (i, i))
+@inline Base.iterate(r::SafeCloseOpen) = (i = Int(first(r)); i ≥ getfield(r, :upper) ? nothing : (i, i))
+@inline Base.iterate(r::AbstractCloseOpen, i::Integer) = (i += one(i)) ≥ getfield(r, :upper) ? nothing : (i, i)
+
+import ArrayInterface
+ArrayInterface.known_first(::Type{<:AbstractCloseOpen{StaticInt{F}}}) where {F} = F
+ArrayInterface.known_step(::Type{<:AbstractCloseOpen}) = 1
+ArrayInterface.known_last(::Type{<:AbstractCloseOpen{<:Any,StaticInt{L}}}) where {L} = L - 1
+ArrayInterface.known_length(::Type{AbstractCloseOpen{StaticInt{F},StaticInt{L}}}) where {F,L} = L - F
+
+Base.IteratorSize(::Type{<:AbstractCloseOpen}) = Base.HasShape{1}()
+Base.IteratorEltype(::Type{<:AbstractCloseOpen}) = Base.HasEltype()
+@inline Base.size(r::AbstractCloseOpen) = (length(r),)
+@inline Base.eltype(r::AbstractCloseOpen) = Int
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using CloseOpenIntervals
 using Test
 
+const ArrayInterface = CloseOpenIntervals.ArrayInterface
 @testset "CloseOpenIntervals.jl" begin
-    # Write your tests here.
+  function mysum(x, N)
+    s = zero(eltype(x))
+    @inbounds @fastmath for i ∈ CloseOpen(N)
+      s += x[i+1]
+    end
+    s
+  end
+  function mysafesum(x, N)
+    s = zero(eltype(x))
+    @inbounds @fastmath for i ∈ SafeCloseOpen(N)
+      s += x[i+1]
+    end
+    s
+  end
+  x = rand(128);
+  for n ∈ 1:128
+    @test mysum(x,n) ≈ mysafesum(x,n) ≈ sum(view(x,1:n))
+    @test length(CloseOpen(n)) == n
+  end
+  @test @inferred(mysum(x,0)) == first(x)
+  @test @inferred(mysafesum(x,0)) == 0.0
+  @test @inferred(mysum(x, CloseOpenIntervals.StaticInt(128))) ≈ sum(x)
+  @test @inferred(ArrayInterface.static_length(CloseOpen(CloseOpenIntervals.StaticInt(128)))) === CloseOpenIntervals.StaticInt(128)
+  @test @inferred(eltype(CloseOpen(7))) === Int
 end
