6fbea346d9ea9cca1df2ab1bf8af07e819319f5d
Documenter.jl <documenter@juliadocs.github.io>
2021-10-25 11:42:42 +0000

    build based on 3a85055

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CloseOpenIntervals","category":"page"},{"location":"#CloseOpenIntervals","page":"Home","title":"CloseOpenIntervals","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CloseOpenIntervals.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CloseOpenIntervals]","category":"page"}]
+}
